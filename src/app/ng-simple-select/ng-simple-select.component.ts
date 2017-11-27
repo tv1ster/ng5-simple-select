2f41fd30ccf9c5b44c9822f647c7b0050fed8bda
Vadym <vadym.ruchka@gmail.com>
2017-11-27 22:28:50 +0200

    Showing/hiding dropdonw on click
    Also hiding on outside click

--- src/app/ng-simple-select/ng-simple-select.component.ts
+++ src/app/ng-simple-select/ng-simple-select.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnChanges, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'ng-simple-select',
   templateUrl: './ng-simple-select.component.html',
-  styleUrls: ['ng-simple-select.component.scss']
+  styleUrls: ['ng-simple-select.component.scss'],
+  host: {
+    '(document:click)': 'onDocClick($event)',
+  }
 })
-export class NgSimpleSelectComponent implements OnInit {
+export class NgSimpleSelectComponent implements OnInit, OnChanges {
+  public showDropdown: boolean;
 
-  constructor() { }
+  constructor(private element: ElementRef) {
+  }
+
+  ngOnChanges() {
+  }
 
   ngOnInit() {
+  }
+
+  public toggleDropdown() {
+    this.showDropdown = !this.showDropdown;
+  }
+
+  private onDocClick (ev: Event) {
+    if (this.isClickOutsideComponent(ev)) {
+      this.showDropdown = false;
+    }
+  }
+
+  private isClickOutsideComponent(ev: Event) {
+    let target = ev.target;
+
+    // checking only 10 parents
+    let i = 0;
+    while (target.parentNode && i < 10) {
+      if (target !== this.element.nativeElement) {
+        target = target.parentNode;
+        i ++;
+      } else {
+        return false;
+      }
+    }
+    return true;
   }
 
 }
