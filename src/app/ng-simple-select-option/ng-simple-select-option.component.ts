55b07536219f03c5a4394f59d2ac5e66b73dcc08
Vadym <vadym.ruchka@gmail.com>
2017-11-29 22:25:23 +0200

    Adding some demo

--- src/app/ng-simple-select-option/ng-simple-select-option.component.ts
+++ src/app/ng-simple-select-option/ng-simple-select-option.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input, ElementRef } from '@angular/core';
 export class NgSimpleSelectOptionComponent implements OnInit {
   @Input() value?: string;
   private parentChangeFn: (value: any) => void;
-  private highlighted: boolean = false;
+  public highlighted: boolean = false;
 
   constructor(private element: ElementRef) { }
 
